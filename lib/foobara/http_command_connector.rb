b7dfc8dd22dd804ba39d1d3d4e5f7c4b2acea6be
Miles Georgi <azimux@gmail.com>
2024-10-25 13:38:34 -0700

    Remove monorepo project wiring

--- lib/foobara/http_command_connector.rb
+++ lib/foobara/http_command_connector.rb
@@ -1,11 +1,4 @@
 require "foobara/all"
 require "foobara/command_connectors"
 
-module Foobara
-  module CommandConnectorsHttp
-  end
-
-  Monorepo.project :command_connectors_http
-end
-
 Foobara::Util.require_directory "#{__dir__}/../../src"
